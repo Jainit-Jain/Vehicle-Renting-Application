e20b6139ab5f9f36974917dcb5a1862122120ca0
Jainit-Jain <jainit9742193087@gmail.com>
2024-12-11 13:16:05 +0530

    adding the user ressgistration feature

--- src/main/java/com/example/vehiclerentingapplication/repository/UserRepository.java
+++ src/main/java/com/example/vehiclerentingapplication/repository/UserRepository.java
@@ -1,14 +1,11 @@
 package com.example.vehiclerentingapplication.repository;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.example.vehiclerentingapplication.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-	@Query("SELECT u FROM User u WHERE u.username=:username")
-	Optional<User>findByUsername(String userName);
+	
 }
