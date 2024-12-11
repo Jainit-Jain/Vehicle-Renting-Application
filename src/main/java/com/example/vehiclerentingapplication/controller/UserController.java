89db86cfa478bc6af89677e9ec6f1217a88c1813
Jainit-Jain <jainit9742193087@gmail.com>
2024-12-11 13:35:39 +0530

    adding the user ressgistration feature

---
+++ src/main/java/com/example/vehiclerentingapplication/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.vehiclerentingapplication.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.vehiclerentingapplication.entity.User;
+import com.example.vehiclerentingapplication.service.UserService;
+import com.example.vehiclerentingapplication.util.ResponseStructure;
+
+@RestController
+public class UserController {
+
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<ResponseStructure<User>> saveUser(@RequestBody User user) {
+		user = userService.saveUser(user);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(ResponseStructure.create(HttpStatus.CREATED.value(), "User Created", user));
+	}
+}
