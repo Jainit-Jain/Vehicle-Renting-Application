8465c104381c4d698f559d0c0b683e52eff83827
Jainit-Jain <jainit9742193087@gmail.com>
2024-12-12 16:43:14 +0530

    adding the dto's

--- src/main/java/com/example/vehiclerentingapplication/service/UserService.java
+++ src/main/java/com/example/vehiclerentingapplication/service/UserService.java
@@ -1,32 +1,27 @@
 package com.example.vehiclerentingapplication.service;
 
-import java.io.IOException;
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.example.vehiclerentingapplication.entity.Image;
 import com.example.vehiclerentingapplication.entity.User;
-import com.example.vehiclerentingapplication.exception.FailedToUploadImageException;
-import com.example.vehiclerentingapplication.exception.UserNotFoundByIdException;
-import com.example.vehiclerentingapplication.repository.ImageRepository;
+import com.example.vehiclerentingapplication.mapper.UserMapper;
 import com.example.vehiclerentingapplication.repository.UserRepository;
+import com.example.vehiclerentingapplication.request.UserRequest;
+import com.example.vehiclerentingapplication.response.UserResponse;
 
 @Service
 public class UserService {
 
 	private final UserRepository userRepository;
+	private final UserMapper userMapper;
 
-	private final ImageRepository imageRepository;
-
-	public UserService(UserRepository userRepository, ImageRepository imageRepository) {
+	public UserService(UserRepository userRepository,UserMapper userMapper) {
 		super();
 		this.userRepository = userRepository;
-		this.imageRepository = imageRepository;
+		this.userMapper=userMapper;
 	}
 
-	public User saveUser(User user) {
-		return userRepository.save(user);
-	}
+	 public UserResponse saveUser(UserRequest userRequest) {
+	        User user = userMapper.mapToUser(userRequest);
+	        User savedUser = userRepository.save(user);
+	        return userMapper.mapToResponse(savedUser);
+	    }
 }
