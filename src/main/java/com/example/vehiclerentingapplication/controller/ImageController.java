7e7c27d3f6e96443815ff801c5708767d14b0615
Jainit-Jain <jainit9742193087@gmail.com>
2024-12-12 14:25:55 +0530

    finding the image with the given id

--- src/main/java/com/example/vehiclerentingapplication/controller/ImageController.java
+++ src/main/java/com/example/vehiclerentingapplication/controller/ImageController.java
@@ -3,12 +3,15 @@ package com.example.vehiclerentingapplication.controller;
 import java.io.IOException;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.vehiclerentingapplication.entity.Image;
 import com.example.vehiclerentingapplication.service.ImageService;
 import com.example.vehiclerentingapplication.util.SimpleResponseStructure;
 
@@ -28,5 +31,14 @@ public class ImageController {
 		imageService.addUserProfilePicture(userId, file);
 		return ResponseEntity.status(HttpStatus.OK)
 				.body(SimpleResponseStructure.create(HttpStatus.OK.value(), "Profile picture added successfully"));
+	}
+
+	@GetMapping("/find/imageById")
+	public ResponseEntity<byte[]> findImageById(@RequestParam int imageId) {
+		Image image = imageService.findImageById(imageId);
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.contentType(MediaType.valueOf(image.getContentType()))
+				.body(image.getImageBytes());
 	}
 }
